e52d1315c98d0ab97f6c9dde6e21cf560548e70a
mike korakakis <mike.korakakis@gmail.com>
2022-02-12 23:28:21 -0800

    removed python files

--- create_cat_slug.py
+++
@@ -1,7 +0,0 @@
-from transliterate import translit, get_available_language_codes
-from slugify import slugify
-
-text = "Δαγκάνες/Εμβολάκια"
-# text = translit(text, 'el', reversed=True)
-
-print(slugify(text))
